f4a8ea6bce681ab207dd0436c2829783e2c55af7
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-07 22:42:34 -0500

    deleting project and redoing

--- youtube-exercise/noteable/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-// This is the master route file , we will bring all the different routes here 
-
-const noteRoutes = require('./note_routes');
-
-module.exports = function(app, db) {
-	noteRoutes(app,db);
-
-}
--- youtube-exercise/noteable/app/routes/notes_routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app, db){
-  app.post('/notes',(req,res)=>{
-    // create teh note here 
-    res.send('hello')
-  })
-}
