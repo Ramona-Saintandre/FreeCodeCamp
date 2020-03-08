89471d260c1ebd407f7678c26df419b1fdd1624d
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-07 23:28:26 -0500

    deleted note

--- noteable/app/routes/index.js
+++
@@ -1,7 +0,0 @@
-//exporting the first route here 
-//this will be our master route file 
-
-const noteRoutes = require('./note_routes');
-module.exports = function(app, db) { 
-   noteRoutes(app, db);
-}
--- noteable/app/routes/note_routes.js
+++
@@ -1,8 +0,0 @@
-//first part of CRUD is create , here we are creating the first route 
-
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) =>{  //not sure where this /notes is coming from 
-    //notes will be created here 
-    res.send('hello')
-  });
-};
