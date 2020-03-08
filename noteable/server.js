afd10627a92820084ec59767526985f29f32df18
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-07 23:05:58 -0500

    changed port to 8080 for postmon

--- noteable/server.js
+++ noteable/server.js
@@ -4,6 +4,6 @@ const bodyParser     = require('body-parser');
 
 const app = express();
 
-const port = 8000;
+const port = 8080;
 app.listen(port, () => {  
   console.log('We are live on ' + port);});
