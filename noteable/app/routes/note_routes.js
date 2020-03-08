8d7ed3a1071f0b0a086d0d0a34d08bb056bfdbdb
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-07 23:01:06 -0500

    set the response in the note_routes

--- noteable/app/routes/note_routes.js
+++ noteable/app/routes/note_routes.js
@@ -1,5 +1,8 @@
 //first part of CRUD is create , here we are creating the first route 
 
 module.exports = function(app, db) {
-  
+  app.post('/notes', (req, res) =>{
+    //notes will be created here 
+    res.send('hello')
+  })
 };
