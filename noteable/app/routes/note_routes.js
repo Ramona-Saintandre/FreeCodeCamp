9db143a206ffc9b2dd57f32b9d69b05629cfa6ba
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-07 23:13:22 -0500

    postman states that cannot Post /note

--- noteable/app/routes/note_routes.js
+++ noteable/app/routes/note_routes.js
@@ -1,8 +1,8 @@
 //first part of CRUD is create , here we are creating the first route 
 
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) =>{
+  app.post('/notes', (req, res) =>{  //not sure where this /notes is coming from 
     //notes will be created here 
     res.send('hello')
-  })
+  });
 };
